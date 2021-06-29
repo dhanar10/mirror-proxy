2ad5a9942bf67ef6855b8d225905450f047e7302
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2021-06-29 08:28:22 +0700

    Improve logic

--- mirror-proxy.py
+++ mirror-proxy.py
@@ -1,56 +1,50 @@
 import http.server
-import socketserver
-import urllib.request
-import shutil
 import os
-import hashlib
+import shutil
+import socketserver
 import sys
-
-from urllib.parse import urlparse
+import urllib.request
 from pathlib import Path
+from urllib.parse import urlparse
 
-class CacheHandler(http.server.SimpleHTTPRequestHandler):
+
+class MirrorHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-      o = urlparse(self.path)
-      cache_filename = Path(os.getcwd() + "/" + o.netloc + o.path)
-      
-      if not os.path.exists(cache_filename):
-        print("MISS", cache_filename)
-        cache_filename.parent.mkdir(parents=True, exist_ok=True)
+      url = urlparse(self.path)
+      mpath = Path(os.getcwd() + "/" + url.netloc + url.path)
+      if not os.path.exists(mpath):
+        print("MISS", mpath)
+        mpath.parent.mkdir(parents=True, exist_ok=True)
         req = urllib.request.Request(self.path)
-        for k in self.headers:
-          if k not in ["Host"]:
-            req.add_header(k, self.headers[k])
-        try:
-          resp = urllib.request.urlopen(req)
-          with open(cache_filename.with_suffix(".tmp"), "wb") as output:
-            buf = resp.read(10240)
-            self.send_response(200)
-            for k in resp.headers:
-              if k in ["Content-Length"]: # Content-Length required by apt update?
-                self.send_header(k,resp.headers[k])
-            self.end_headers()
-            while buf:
-              output.write(buf)
-              self.wfile.write(buf)
-              buf = resp.read(10240)
-          os.rename(cache_filename.with_suffix(".tmp"), cache_filename)
-        except urllib.error.HTTPError as err:
-          self.send_response(err.code)
-          self.end_headers()
-          return
+        for h in self.headers:
+          if h not in ["Host"]:
+            req.add_header(h, self.headers[h])
+        res = urllib.request.urlopen(req)
+        buf = res.read(10240)
+        self.send_response(200)
+        for h in res.headers:
+          if h in ["Content-Length"]: # Content-Length is required by some apps e.g. apt-get
+            self.send_header(h, res.headers[h])
+        self.end_headers()
+        with open(mpath.with_suffix(".tmp"), "wb") as mfile:
+          while buf:
+            mfile.write(buf)
+            self.wfile.write(buf)
+            buf = res.read(10240)
+        os.rename(mpath.with_suffix(".tmp"), mpath)
       else:
-        print("HIT", cache_filename)
-        with open(cache_filename, "rb") as cached:
+        print("HIT ", mpath)
+        with open(mpath, "rb") as mfile:
           self.send_response(200)
-          self.send_header("Content-Length",os.path.getsize(cache_filename))
+          self.send_header("Content-Length",os.path.getsize(mpath)) # Content-Length is required by some apps e.g. apt-get
           self.end_headers()
-          shutil.copyfileobj(cached, self.wfile)
+          shutil.copyfileobj(mfile, self.wfile)
 
-#socketserver.TCPServer.allow_reuse_address = True # If enabled, port cannot be accessed from inside docker container?
 
-with socketserver.TCPServer(("0.0.0.0", 8000), CacheHandler) as httpd:
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        sys.exit(0)
+if __name__ == '__main__':
+  #socketserver.TCPServer.allow_reuse_address = True # FIXME If enabled, port cannot be accessed from docker container?
+  with socketserver.TCPServer(("0.0.0.0", 8000), MirrorHandler) as httpd:
+      try:
+          httpd.serve_forever()
+      except KeyboardInterrupt:
+          sys.exit(0)
